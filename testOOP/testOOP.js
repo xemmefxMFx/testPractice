d202461486203ea3f0d0ad13d9aa958619a76b8a
xemmefxMFx <femmemexxx@gmail.com>
2022-11-15 15:11:35 +0700

    Create Summary OOP Test file - Halt due to sleepiness

---
+++ testOOP/testOOP.js
@@ -0,0 +1,14 @@
+const Book = function(t, a) {
+    let title = t;
+    let author = a;
+
+    return{
+        summary: function() {
+            console.log(`${title} is written by ${author}.`);
+        }
+    }
+}
+
+const book1 = new Book('Hippie', 'Paulo Coelho');
+
+book1.summary();
