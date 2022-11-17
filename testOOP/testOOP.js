cb3f9a5e386156d71296da22a55c3755556dda21
xemmefxMFx <femmemexxx@gmail.com>
2022-11-17 15:30:36 +0700

    Update testOOP

--- testOOP/testOOP.js
+++ testOOP/testOOP.js
@@ -1,14 +1,26 @@
-const Book = function(t, a) {
-    let title = t;
-    let author = a;
-
-    return{
-        summary: function() {
-            console.log(`${title} is written by ${author}.`);
-        }
+const getTitle = (data) => ({
+    title : () => console.log(`title : ${data.title}`)
+ });
+ const getAuthor = (data) => ({
+    author : () => console.log(`author: ${data.author}`)
+ });
+ const getSummary = () =>  ({
+    summary :() => console.log(`book summary need to update.`)
+ });
+ const Book = (title, author) => {
+    const data = { 
+       title, 
+       author  
     }
-}
-
-const book1 = new Book('Hippie', 'Paulo Coelho');
-
-book1.summary();
+    
+    return Object.assign({},
+              getTitle(data), 
+              getAuthor(data), 
+              getSummary()
+    )
+ }
+ let book1 = Book('The Alchemist', 'Paulo Coelho');
+ book1.title();
+ book1.summary();
+ book1.author();
+ //   "title : The Alchemist"
